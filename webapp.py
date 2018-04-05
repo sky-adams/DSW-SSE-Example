d6578d5183126db05e9aca016cb3eb6d47916540
Sky Adams <skyadams@sbunified.org>
2018-04-04 18:39:21 -0700

    Switched to SocketIO

--- webapp.py
+++ webapp.py
@@ -1,20 +1,38 @@
-from flask import Flask, redirect, url_for, session, request, jsonify, Markup
-from flask import render_template
-from flask_sse import sse
+#adapted from https://github.com/miguelgrinberg/Flask-SocketIO/tree/master/example
+
+from threading import Lock
+from flask import Flask, render_template, session, request
+from flask_socketio import SocketIO, emit, join_room, leave_room, \
+    close_room, rooms, disconnect
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, async_mode=None)
+thread = None
+thread_lock = Lock()
 
-app.debug = True #Change this to False for production
-app.config["REDIS_URL"] = "redis://localhost"
+
+def background_thread():
+    count = 0
+    while True:
+        socketio.sleep(10) #wait 10 seconds
+        count += 1 #add 1 to count
+        socketio.emit('my_response', count) #send count to all clients
 
 @app.route('/')
-def home():
-    return render_template('home.html')
-    
-@app.route('/hello')
-def publish_hello():
-  sse.publish({"message": "Hello!"}, type='greeting')
-  return "Message sent!" #where does this go?
+def index():
+    return render_template('home.html', async_mode=socketio.async_mode)
+
+
+@socketio.on('connect')
+def test_connect():
+    global thread
+    with thread_lock: #lock thread in case multiple clients are connecting at the same time
+        #create one thread for all clients
+        if thread is None:
+            thread = socketio.start_background_task(target=background_thread)
+    emit('start', 'Connected')
+    #to use emit: emit(event, data)
 
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app, debug=True)
