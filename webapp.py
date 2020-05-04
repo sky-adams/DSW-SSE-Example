22932ed78944fc4bff9cd4928fa0e5507cf7c803
sky-adams <skyadams@sbunified.org>
2020-05-04 14:06:25 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -7,16 +7,6 @@ from flask_socketio import SocketIO, emit, join_room, leave_room, \
 
 app = Flask(__name__)
 socketio = SocketIO(app, async_mode=None)
-thread = None
-thread_lock = Lock()
-
-
-def background_thread():
-    count = 0
-    while True:
-        socketio.sleep(5) #wait 5 seconds
-        count += 1 #add 1 to count
-        socketio.emit('count_event', count) #send count to ALL clients (see Broadcasting section of https://flask-socketio.readthedocs.io/en/latest/)
 
 @app.route('/')
 def index():
@@ -25,13 +15,10 @@ def index():
 
 @socketio.on('connect')
 def test_connect():
-    global thread
-    with thread_lock: #lock thread in case multiple clients are connecting at the same time
-        #create one thread for all clients
-        if thread is None:
-            thread = socketio.start_background_task(target=background_thread)
-    emit('start', 'Connected') #send data to the client that just connected
+    new_user = request.args['username']
+    emit('start', 'You joined as ' + new_user) #send data to the client that just connected
     #to use emit: emit(event, data)
+    socketio.emit('new_user_event', new_user) #send data to ALL clients
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
